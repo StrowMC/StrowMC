3d203773f0facfa45f0e8cb00f502b2f4a6849a1
Hokkaydo <alexandre.orekhoff@gmail.com>
2020-07-05 20:10:48 +0200

    First PunishmentModule commit

---
+++ StrowCore/src/main/java/fr/strow/core/module/punishment/command/TempMuteCommand.java
@@ -0,0 +1,20 @@
+package fr.strow.core.module.punishment.command;
+
+
+import me.choukas.commands.EvolvedCommand;
+import me.choukas.commands.api.CommandDescription;
+
+/**
+ * Created by Hokkaydo on 27-06-2020.
+ */
+public class TempMuteCommand extends EvolvedCommand {
+
+    public TempMuteCommand(CommandDescription description) {
+        super(description);
+    }
+
+    @Override
+    protected void define() {
+
+    }
+}
